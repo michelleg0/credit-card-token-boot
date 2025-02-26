34f647eb5a6cd940da67ce6780eeb59eebe66597
sdk <s.danyal.k@gmail.com>
2025-02-26 16:08:53 -0700

    initial commit

---
+++ src/main/java/com/sdk/creditcardtokenboot/CreditCardTokenBootApplication.java
@@ -0,0 +1,13 @@
+package com.sdk.creditcardtokenboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardTokenBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardTokenBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdk/creditcardtokenboot/CreditCardTokenBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdk.creditcardtokenboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardTokenBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
